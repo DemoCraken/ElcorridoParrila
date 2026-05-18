2abf24a66968cf8bd16f4b7b1a466fe67d0a4be6
DemoCraken <162372574+DemoCraken@users.noreply.github.com>
2026-05-18 00:15:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,79 @@ function cerrarPopup() {
 document.addEventListener("DOMContentLoaded", () => {
     mostrarPopup();
 });
+
+
+
+
+tsParticles.load("tsparticles", {
+    background: {
+        color: "transparent"
+    },
+
+    fpsLimit: 60,
+
+    particles: {
+
+        number: {
+            value: 0
+        },
+
+        color: {
+            value: [
+                "#f4b942",
+                "#ff7b00",
+                "#ffffff"
+            ]
+        },
+
+        shape: {
+            type: "circle"
+        },
+
+        opacity: {
+            value: 1
+        },
+
+        size: {
+            value: {
+                min: 2,
+                max: 4
+            }
+        },
+
+        move: {
+            enable: true,
+            speed: 8,
+            direction: "none",
+            outModes: {
+                default: "destroy"
+            }
+        }
+    },
+
+    emitters: [
+        {
+            position: {
+                x: 20,
+                y: 80
+            },
+
+            rate: {
+                quantity: 3,
+                delay: 0.4
+            }
+        },
+
+        {
+            position: {
+                x: 80,
+                y: 20
+            },
+
+            rate: {
+                quantity: 3,
+                delay: 0.5
+            }
+        }
+    ]
+});
